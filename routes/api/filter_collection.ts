0b7e82c2cb0264193d2b74a8e3bf17a7fd99fdc6
alessandrokonrad <akonrad6@gmail.com>
2024-10-01 16:16:38 +0200

    config updates

--- routes/api/filter_collection.ts
+++ routes/api/filter_collection.ts
@@ -1,16 +1,11 @@
 import { FreshContext } from "$fresh/server.ts";
-import {
-  filterCollection,
-  MetadataCollection,
-} from "@/lib/filter_collection.ts";
-import metadata from "https://raw.githubusercontent.com/spacebudz/wormhole/refs/heads/main/artifacts/metadata.json" with {
-  type: "json",
-};
+import { filterCollection } from "@/lib/filter_collection.ts";
+import { metadataCollection } from "@/lib/metadata.ts";
 
 export const handler = (req: Request, _ctx: FreshContext): Response => {
   const params = new URLSearchParams(req.url);
   const result = filterCollection({
-    metadataCollection: metadata as MetadataCollection,
+    metadataCollection,
     params,
     page: parseInt(params.get("page") || "0"),
   });
